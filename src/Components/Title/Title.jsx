d5bf36d95d55ea48831fd3dca76906f9a191dec3
khibe <bryanirha1@gmail.com>
2024-04-29 10:44:28 -0700

    First commits

---
+++ src/Components/Title/Title.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Title.css'
+
+const Title = () => {
+  return (
+    <div className='title'>
+        <p>OUR PROGRAM</p>
+        <h2>What We Offer</h2>
+      
+    </div>
+  )
+}
+
+export default Title
